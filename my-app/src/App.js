38cf09d91bb88d5380af9a1a31668f303a572118
Alfonso Arriola <alfonsoarriolajr@gmail.com>
2018-08-19 15:04:08 -0400

    edia query update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import List from './List';
 import Flexbox from 'flexbox-react';
-import Background from './jay-z-fan-art-josh-little.jpg';
+import Background from './jay2.jpg';
 
-var sectionStyle = {
-  width: "100%",
-  height: "400px",
+let sectionArt = {
+  width: "80%",
+  height: "40rem",
   backgroundImage: `url(${Background})`,
-  backgroundSize: 'cover'
+  backgroundSize: 'cover',
+  backgroundPosition: 'center'
 };
 
 
@@ -37,20 +38,29 @@ export default class App extends Component {
     return (
       <div>
         <Flexbox flexDirection="column" minHeight="100vh">
-          <Flexbox element="header" height="60px" paddingTop="10px" paddingBottom="10px" margin="10px">
-            <h1> 99 Problems, but a REACT TODO list aint one </h1>
+          <Flexbox element="header" justifyContent="center" height="60px" paddingTop="10px" paddingBottom="10px" margin="30px">
+            <h1> 
+               <div>
+                 I Got 99 Problems,
+               </div>
+
+               <div>
+                But React Ain't One
+               </div>
+            </h1>
           </Flexbox>
          
          <Flexbox flexGrow={1} padding="10px">
              <div ClassName="list">
                 <form className="App" onSubmit={this.onSubmit}>
+                  <p> <u> PROBLEMS </u> </p>
                   <input value={this.state.term} onChange={this.onChange} />
                   <button>Submit</button>
                 </form>
                 <List items={this.state.items} />
               </div>
 
-              <div ClassName="art" style={ sectionStyle }>
+              <div ClassName="art" style={ sectionArt }>
                 
               </div>            
           </Flexbox>
